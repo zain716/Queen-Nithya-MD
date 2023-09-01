132c21afbdbec12a1c5721ce4867fe27593cbf3c
zain716 <142066969+zain716@users.noreply.github.com>
2023-09-01 21:08:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ const fs = require('fs-extra')
   
   
  //═══════[Required Variables]════════\\ 
- global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.owner = process.env.OWNER_NUMBER.split("923085641185") 
  global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/nipuna15' 
  global.location = 'Sri lanka, Monaragala' 
  global.gurl = 'https://instagram.com/' // add your username 
- global.sudo = process.env.SUDO || '919628516236' 
- global.devs = '919628516236'; 
- global.website = 'https://github.com/' //wa.me/+91000000000000 
+ global.sudo = process.env.SUDO || '923085641185' 
+ global.devs = '923085641185'; 
+ global.website = 'https://github.com/' //wa.me/+923085641185 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' 
  module.exports = { 
    botname:   process.env.BOT_NAME === undefined ? '' : process.env.BOT_NAME, 
